454b004923c9f26f94bec02ed5461a372919eacf
N. M. Ali Hayder <ahayder@loobylab.com>
2016-05-14 16:32:07 +0600

    Initial Commit

---
+++ Country&Capital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Country&Capital
+//
+//  Created by N. M. Ali Hayder on 5/14/16.
+//  Copyright © 2016 N. M. Ali Hayder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
